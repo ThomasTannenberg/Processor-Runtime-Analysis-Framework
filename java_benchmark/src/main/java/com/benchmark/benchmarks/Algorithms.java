aa1109977deac80e2e4ccb8987dd3a032c76e3f5
Thomas Tannenberg <Thomas@Tannenberg.online>
2025-06-26 09:06:27 +0200

    adoptation of new folder and file structure design

---
+++ java_benchmark/src/main/java/com/benchmark/benchmarks/Algorithms.java
@@ -0,0 +1,5 @@
+package com.benchmark.benchmarks;
+
+public class Algorithms {
+    
+}
---
+++ rust_benchmark/src/benchmarks/algorithms.rs
@@ -0,0 +1,4 @@
+fn nothing_here_output() {
+    let message = "there is nothing here yet, but there will be soon! Maybe";
+    println!("{}", message);
+}
